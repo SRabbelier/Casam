c0aabcde223d96d7a141763476fae98cfd9ffbe9
srabbelier <srabbelier@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-04-14 14:16:35 +0000

    UUID field gebruiken
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@157 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/casam/models.py
+++ Code/casam_django/casam/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+from django_tools import fields
+
 class Patient(models.Model):
+  id = fields.UUIDField(primary_key=True)
   corpse_id = models.IntegerField()
   sex = models.BooleanField()
 
 class Image(models.Model):
+  id = fields.UUIDField(primary_key=True)
   path = models.CharField(max_length=100)
   name = models.CharField(max_length=30)
   added = models.DateField(auto_now_add=True)
