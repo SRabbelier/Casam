d788d3b8e09d314750636da23d18efa1943daeb3
jaapdh <jaapdh@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-04-16 13:13:07 +0000

    handige linkjes en een overzicht
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@188 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/casam/views/main.py
+++ Code/casam_django/casam/views/main.py
@@ -1,7 +1,9 @@
 from django import http
 from django.template import loader
+from casam.models import OriginalImage
+from casam.models import Project
 
 def home(request):
-  context = {'name': 'ben'}
+  context = {'name': 'ben','images':OriginalImage.objects.all(), 'projects': Project.objects.all()}
   content = loader.render_to_string('main/home.html', dictionary=context)
   return http.HttpResponse(content)
