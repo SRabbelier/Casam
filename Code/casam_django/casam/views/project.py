b5f02accc9fb5b9f1af4f6502586c31ce8fea277
srabbelier <srabbelier@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-04-17 12:26:50 +0000

    alleen images voor deze project
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@219 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/casam/views/project.py
+++ Code/casam_django/casam/views/project.py
@@ -1,10 +1,14 @@
+import uuid
+
 from django import http
 from django.template import loader
 from casam.models import OriginalImage
 from casam.models import Project
 
+
 def home(request, id):
-  img = OriginalImage.objects.select_related().order_by('project__name')
+  id = uuid.UUID(id)
+  img = OriginalImage.objects.select_related().order_by('project__name').filter(project__id=id)
 
   context = {'images':img}
 
