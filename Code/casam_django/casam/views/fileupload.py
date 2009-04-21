91b4a55c51b3b2607afcd01bec6ca0f8ca537487
srabbelier <srabbelier@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-04-21 11:47:46 +0000

    Logic in eigen module plx
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@268 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/casam/views/fileupload.py
+++ Code/casam_django/casam/views/fileupload.py
@@ -1,19 +1,24 @@
-from django import http
-from django.template import loader
-from django import forms
-from ..models import Image
-from ..models import Patient
-from ..models import Project
-from ..models import OriginalImage
-from django.conf import settings
-
-import uuid
-import time
 import mimetypes
 import os
+
 import Image
 
+from django import forms
+from django import http
+from django.conf import settings
+from django.template import loader
+
+from casam.logic import fileupload as fileupload_logic
+from casam.models import Image
+from casam.models import Patient
+from casam.models import Project
+from casam.models import OriginalImage
+
+
 class UploadFileForm(forms.Form):
+  """TODO: dosctring
+  """
+
   is_left = forms.CharField(max_length=5,widget=forms.RadioSelect(choices=((True,"Links"),(False,"Rechts"))))
 
   name = forms.CharField(max_length=50)
@@ -21,11 +26,16 @@ class UploadFileForm(forms.Form):
 
 
 def fileupload(request, id_str):
+  """TODO: Docstring
+  """
+
   if request.method == 'POST':
     form = UploadFileForm(request.POST, request.FILES)
 
     if form.is_valid():
-      oi = handle_uploaded_file(request.FILES['file'],request.POST, id_str)
+      oi = fileupload_logic.handle_uploaded_file(request.FILES['file'],
+                                                 request.POST, id_str)
+
       DATADIR = "../"+getattr(settings, 'DATADIR')
       context = {'image': oi, 'DATADIR':DATADIR}
       content = loader.render_to_string('main/succes.html', dictionary=context)
@@ -39,46 +49,11 @@ def fileupload(request, id_str):
   content = loader.render_to_string('main/fileupload.html', dictionary=context)
   return http.HttpResponse(content)
 
-def handle_uploaded_file(file,post, id_str):
-  DATADIR = getattr(settings, "DATADIR")
-  timestamp = time.time()
-  fileNameOnly = "%d-%s" % (timestamp, file.name)
-  location = DATADIR+"%d-%s" % (timestamp, file.name)
-  destination = open(location, 'wb+') #wb+ is write binary
-  for chunk in file.chunks():
-      destination.write(chunk)
-  destination.close()
-
-  #open the file and create a thumbnail out of it
-  fullImage = Image.open(location)
-  fullImageWidth = fullImage.size[0]
-  fullImageHeight = fullImage.size[1]
-  squareSize = min(fullImageWidth, fullImageHeight)
-  box = ((fullImageWidth-squareSize)/2,(fullImageHeight-squareSize)/2,(fullImageWidth-squareSize)/2+squareSize,(fullImageHeight-squareSize)/2+squareSize)
-  fullImage = fullImage.crop(box)
-
-
-  sizes = 50,100,200,300
-  for singleSize in sizes:
-      thumbnailLocation = DATADIR+"thumbnail/%d/%d-%s" % (singleSize,timestamp, file.name)
-      thumbnail=fullImage.copy()
-      thumbnail.thumbnail((singleSize,singleSize),Image.ANTIALIAS)
-      thumbnail.save(thumbnailLocation)
-
-  #temporarly create a patient object
-  #because we need this info
-  Patient.objects.all()
-  pat = Patient(corpse_id=234,sex=True)
-  pat.save()
-  #safe the uploaded image
-  OriginalImage.objects.all()
-
-  proj = Project.objects.get(id=uuid.UUID(id_str))
-  oi = OriginalImage(patient=pat,name=post['name'],path=fileNameOnly,is_left=post['is_left'],project=proj)
-  oi.save()
-  return oi
 
 def viewfile(request, name):
+  """TODO: Docstring
+  """
+
   mime = mimetypes.MimeTypes
   mime = mime()
   if os.path.exists('data/'+name):
