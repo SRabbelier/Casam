a6b724532541f1f910b2ced550b22896be69003a
srabbelier <srabbelier@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-04-22 09:19:19 +0000

    nasty EOL fixes
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@300 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/casam/views/users.py
+++ Code/casam_django/casam/views/users.py
@@ -13,37 +13,37 @@ from ..models import User
 class UserForm(forms.Form):
   choices = []
   projects = Project.objects.all()
-  
+
   for pr in projects:
     choices.append((pr.id,pr.name))
-  
+
   login = forms.CharField(max_length=30)
-  name = forms.CharField(max_length=100)  
-  type = forms.CharField(max_length=1, widget=forms.Select(choices=(('C', 'Chirurg'),('O', 'Onderzoeker'), ('A', 'Beheerder'))))  
+  name = forms.CharField(max_length=100)
+  type = forms.CharField(max_length=1, widget=forms.Select(choices=(('C', 'Chirurg'),('O', 'Onderzoeker'), ('A', 'Beheerder'))))
   password = forms.CharField(max_length=10, widget=forms.widgets.PasswordInput())
   id = forms.CharField(max_length=40, widget=forms.widgets.HiddenInput(), required=False)
   read = forms.CharField(widget=forms.widgets.SelectMultiple(choices=choices))
-  
+
 def new(request):
   if request.method == 'POST':
     form = UserForm(request.POST)
     if form.is_valid():
       handle_add_user(request.POST)
-      return http.HttpResponseRedirect('/') 
-  
+      return http.HttpResponseRedirect('/')
+
   context = {'form': UserForm()}
   content = loader.render_to_string('user/new.html', dictionary=context)
   return http.HttpResponse(content)
 
 def handle_add_user(post):
-  user = User(login=post['login'], name=post['name'],type=post['type'],password='12345', id=post['id'])  
+  user = User(login=post['login'], name=post['name'],type=post['type'],password='12345', id=post['id'])
   user.save()
   projid = uuid.UUID(post['read'])
   pr = Project.objects.get(id=projid)
   user.read.add(pr)
   print user.read.all()
   user.save()
-  
+
 def view(request, id_str):
   DATADIR = '../'+getattr(settings, 'DATADIR')
   id = uuid.UUID(id_str)
@@ -52,7 +52,7 @@ def view(request, id_str):
   context = {'form': UserForm(initial=initial)}
   content = loader.render_to_string('user/edit.html', dictionary=context)
   return http.HttpResponse(content)
-    
+
 def home(request):
   DATADIR = '../'+getattr(settings, 'DATADIR')
   users = User.objects.all()
