b89ed8fde110ede811069af5bb0221a4aa3c34f3
svanberkel <svanberkel@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-04-23 20:12:55 +0000

    Geen zin meer om eraan te werken vanavond :)
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@361 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/casam/views/sjorsdraw.py
+++ Code/casam_django/casam/views/sjorsdraw.py
@@ -5,4 +5,14 @@ from django.template import loader
 def sjorsDraw(request):
   content = loader.render_to_string('draw/sjorsdrawtest.html')
   
-  return http.HttpResponse(content)   
+  return http.HttpResponse(content)
+  
+def addBrushStroke(request,brushStroke):
+  user = request.user
+  if user.is_authenticated():  
+    
+    wrapper = FileWrapper(file(temporaryImage))
+    response = http.HttpResponse(wrapper, content_type='image/jpeg')
+    response['Content-Length'] = os.path.getsize(temporaryImage)
+  
+    return response
