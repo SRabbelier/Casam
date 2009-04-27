efffa5fdfbbb0c67eb4ebc38fd924b38c4a2be17
bbijl <bbijl@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-04-27 13:40:08 +0000

    New tab-dock with all controls of project page
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@428 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/media/js/images.js
+++ Code/casam_django/media/js/images.js
@@ -1,6 +1,7 @@
 function ChangeOp(id,value){
   $('big_image'+id).setOpacity(value);
 }
+
 function hide_image(id){
   $('big_images').removeChild($('big_image'+id));
   $('delete_images').removeChild($('delete_photo_'+id));
@@ -10,6 +11,7 @@ function hide_image(id){
 }
 
 function show_image(location){
+	
   var insert = 0;
   for(var i = 1; i <= total_images; i++){
     if($('big_image'+i) == null)
@@ -21,16 +23,16 @@ function show_image(location){
   }
 
 
-  //creating the new image
+  // Creating the new image
   var newimg = Element.extend(document.createElement('img'));
   newimg.setAttribute('src',location);
-  newimg.setStyle("position: absolute; top: 25; left: 225; opacity: 0.4; filter: alpha(opacity=40); width: 1000px; cursor: crosshair;");
+  newimg.setStyle("opacity: 0.4; filter: alpha(opacity=40); width: 1000px; cursor: crosshair;");
   newimg.observe('click', function() { LoadMMDD(""); });
   newimg.setAttribute('id','big_image'+insert);
   $('big_images').appendChild(newimg);
 
-  //To do: make the sliders div just like the objects of the delete button
-  //$('sliders').innerHTML = $('sliders').innerHTML+'<div id="track'+insert+'" style="width:200px; background-color:#ccc; height:10px;"><div id="handle'+insert+'" style="width:10px; height:15px; background-color:#f00; cursor:move;"></div></div><p id="valueopac'+insert+'">&nbsp;</p>';
+  // To do: make the sliders div just like the objects of the delete button
+  // $('sliders').innerHTML = $('sliders').innerHTML+'<div id="track'+insert+'" style="width:200px; background-color:#ccc; height:10px;"><div id="handle'+insert+'" style="width:10px; height:15px; background-color:#f00; cursor:move;"></div></div><p id="valueopac'+insert+'">&nbsp;</p>';
   var newdiv1 = Element.extend(document.createElement('div'));
   newdiv1.setAttribute('id','track'+insert);
   newdiv1.setStyle("width:200px; background-color:#ccc; height:10px;");
@@ -44,7 +46,7 @@ function show_image(location){
   $('sliders').appendChild(newp1);
   eval("new Control.Slider('handle"+insert+"', 'track"+insert+"', {onSlide: function(v) { $('valueopac"+insert+"').innerHTML = 'slide: ' + v*100; ChangeOp("+insert+",v) },onChange: function(v) { $('valueopac"+insert+"').innerHTML = 'changed: ' + v*100;  ChangeOp("+insert+",v) },sliderValue: 0.4})");
       
-  //creating the delete button
+  // Creating the delete button
   var newbutton = Element.extend(document.createElement('button'));
   newbutton.innerHTML = 'Reset foto '+insert;
   newbutton.onclick = function() { hide_image(insert) };
