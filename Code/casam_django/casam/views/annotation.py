b9d69c32075f5629cc65d230a97fe1822a6ee72f
srabbelier <srabbelier@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-04-28 09:38:03 +0000

    Kiezen between url en file annotation
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@442 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/casam/views/annotation.py
+++ Code/casam_django/casam/views/annotation.py
@@ -1,4 +1,5 @@
 import uuid
+import urllib
 
 from django import http
 from django import forms
@@ -10,10 +11,24 @@ from casam.views import handler
 from casam.models import Annotation
 
 
-class ProjectForm(forms.Form):
+CHOICES = [
+    ('url', 'Use an existing url'),
+    ('file', 'Upload a new file'),
+    ]
+
+
+class SelectTypeForm(forms.Form):
+  type = forms.ChoiceField(CHOICES)
+
+
+class FileAnnotationForm(forms.Form):
+  name = forms.CharField(max_length=50)
+  file = forms.FileField()
+
+
+class UrlAnnotationForm(forms.Form):
   name = forms.CharField(max_length=50)
   url = forms.URLField()
-  file = forms.FileField()
 
 
 class ViewAnnotation(handler.Handler):
@@ -58,10 +73,22 @@ class NewAnnotation(handler.Handler):
   """
 
   def getGetForm(self):
-    return ProjectForm()
+    if self.GET.get('type') == 'url':
+      return UrlAnnotationForm()
+
+    if self.GET.get('type') == 'file':
+      return FileAnnotationForm()
+
+    return SelectTypeForm()
 
   def getPostForm(self):
-    return ProjectForm(self.POST, self.FILES)
+    if self.GET.get('type') == 'url':
+      return UrlAnnotationForm(self.POST)
+
+    if self.GET.get('type') == 'file':
+      return FileAnnotationForm(self.POST, self.FILES)
+
+    return SelectTypeForm(self.POST)
 
   def get(self):
     context = self.getContext()
@@ -70,12 +97,25 @@ class NewAnnotation(handler.Handler):
     return http.HttpResponse(content)
 
   def post(self):
+    annotation_type = self.GET.get('type')
+
+    if annotation_type not in ['file', 'url']:
+      annotation_type = self.cleaned_data['type']
+      url = self.path + '?' + urllib.urlencode({'type': annotation_type})
+      return http.HttpResponseRedirect(url)
+
     project_id = self.kwargs['uuid']
     name = self.cleaned_data['name']
-    url = self.cleaned_data['url']
-    file = self.FILES['file']
+
+    if annotation_type == 'url':
+      url = self.cleaned_data['url']
+
+    if annotation_type == 'file':
+      file = self.FILES['file']
+      location, _, _ = fileupload_logic.handle_uploaded_file(file, name)
+      url = self.BASE_PATH + location
+
     annotation_logic.handle_add_annotation(name, url, project_id)
-    oi = fileupload_logic.handle_uploaded_file(file, name)
     context = self.getContext()
 
     return http.HttpResponseRedirect(context['BASE_PATH']+'home') 
