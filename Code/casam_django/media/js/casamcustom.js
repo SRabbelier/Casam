487a66ef0d9e422cd33d50bd3b2865a8eb5978f9
svanberkel <svanberkel@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-04-29 13:43:31 +0000

    Onscreen toevoegen van plaatjes (Nog lelijk)
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@458 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/media/js/casamcustom.js
+++ Code/casam_django/media/js/casamcustom.js
@@ -14,3 +14,32 @@ Element.addMethods({
                 element.setStyle({height: height});
                 return element;
         }});
+
+function popupIFrame(url,width,height){
+	if(!width)width=500;
+	if(!height)height=300;
+	
+	var iF = new Element('iframe', {'src':url});
+	$('popup').update(iF);
+	
+	$('popup').setWidth(width);
+	$('popup').setHeight(height);
+	$('popup').setStyle({
+		zIndex:9999,
+		top:((document.viewport.getHeight() - $('popup').getHeight())/2)+'px',
+		left:((document.viewport.getWidth() - $('popup').getWidth())/2)+'px'
+		});
+	
+}
+
+function closePopup(){
+	$('popup').setWidth(0);
+	$('popup').setHeight(0);
+	$('popup').setStyle({
+		zIndex:0,
+		top:'0px',
+		left:'0px'
+		});
+
+}
+
