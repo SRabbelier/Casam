7e26667b50e6162486ab73651e73b856e3990cb3
srabbelier <srabbelier@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-05-04 12:52:30 +0000

    vrolijk tag selectie dingetje, looks ugly
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@484 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/casam/views/main.py
+++ Code/casam_django/casam/views/main.py
@@ -8,6 +8,8 @@ from django.template import loader
 from casam.models import Project
 from casam.models import OriginalImage
 from casam.views import handler
+from casam.views import tag as tag_view
+
 
 class Home(handler.Handler):
   """Handler for the home page.
@@ -24,6 +26,7 @@ class Home(handler.Handler):
       projects[project] = imgs[0] if imgs else ''
 
     context['projects'] = projects
+    context['tag_form'] = tag_view.SelectTagForm()
 
     content = loader.render_to_string('main/home.html', dictionary=context)
     return http.HttpResponse(content)
