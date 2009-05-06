acf976dc2f32d37996919e82009aa552a5894a52
bmwsedee <bmwsedee@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-05-06 14:17:38 +0000

    coordinate fix (again)
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@519 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/media/js/getmouse.js
+++ Code/casam_django/media/js/getmouse.js
@@ -1,27 +1,13 @@
 function getMouseXY(e) {
 
-  if($('big_image1') != null){
-    var BORDER_ADJUSTMENT = 2 // px
-    //ugly IE fix for getting the value of the mouse
-    //the '-2' fix is to correct for the borders
-    if(document.all){
-      mX = event.clientX-BORDER_ADJUSTMENT
-      mY = event.clientY-BORDER_ADJUSTMENT
-    }
-    else{
-      mX = e.clientX
-      mY = e.clientY
-    }
-    if( mX-$('big_image1').offsetParent.offsetLeft >=0 &&
-        mX-$('big_image1').offsetParent.offsetLeft <= $('big_image1').width &&
-        mY-$('big_image1').offsetParent.offsetTop >=0 &&
-        mY-$('big_image1').offsetParent.offsetTop <= $('big_image1').height
-      ){      
-      $('MouseX').value = mX-$('big_image1').offsetParent.offsetLeft;
-      $('MouseY').value = mY-$('big_image1').offsetParent.offsetTop;
-      return;
-    }
+  if($('big_images').firstChild != null){
+    mX = e.clientX
+    mY = e.clientY
+  
+      viewportOffset = $('big_images').viewportOffset();    
+      $('MouseX').value = mX-viewportOffset.left;
+      $('MouseY').value = mY-viewportOffset.top;
+ 
   }
-  $('MouseX').value = "";
-  $('MouseY').value = "";
+
 }
