a158ba61fb73c337390ae9de18a95fd293b99538
bmwsedee <bmwsedee@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-05-06 11:59:42 +0000

    IE coordinates fix
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@508 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/media/js/getmouse.js
+++ Code/casam_django/media/js/getmouse.js
@@ -1,20 +1,22 @@
 function getMouseXY(e) {
 
   if($('big_image1') != null){
+    var BORDER_ADJUSTMENT = 2 // px
     //ugly IE fix for getting the value of the mouse
+    //the '-2' fix is to correct for the borders
     if(document.all){
-      mX = event.clientX
-      mY = event.clientY
+      mX = event.clientX-BORDER_ADJUSTMENT
+      mY = event.clientY-BORDER_ADJUSTMENT
     }
     else{
-      mX = e.pageX
-      mY = e.pageY
+      mX = e.clientX
+      mY = e.clientY
     }
     if( mX-$('big_image1').offsetParent.offsetLeft >=0 &&
         mX-$('big_image1').offsetParent.offsetLeft <= $('big_image1').width &&
         mY-$('big_image1').offsetParent.offsetTop >=0 &&
         mY-$('big_image1').offsetParent.offsetTop <= $('big_image1').height
-      ){
+      ){      
       $('MouseX').value = mX-$('big_image1').offsetParent.offsetLeft;
       $('MouseY').value = mY-$('big_image1').offsetParent.offsetTop;
       return;
