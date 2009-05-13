05ea7d9f2885860201fb79468b2387caf13cf9d1
nnsmit <nnsmit@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-05-13 10:50:53 +0000

    Further VTK experiments
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@561 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/casam/logic/point_distribution_model.py
+++ Code/casam_django/casam/logic/point_distribution_model.py
@@ -1,112 +1,43 @@
-# File:        sphere2.py
-
-# import python module
 import vtk
 
 # our data source, a PolyDataReader
-reader = vtk.vtkPolyDataReader()
-reader.SetFileName("H:/desktop/sphere.vtk")
+#reader = vtk.vtkPolyDataReader()
+reader = vtk.vtkUnstructuredGridReader()
+reader.SetFileName("H:/desktop/landmarks.vtk")
 reader.Update() # by calling Update() we read the file
 
 # find the range of the point scalars
-pa,pb = reader.GetOutput().GetPointData().GetScalars().GetRange()
-pnm =  reader.GetOutput().GetPointData().GetScalars().GetName()
-print "Range of %s: %4.2f-%4.2f" %(pnm,pa,pb)
+pointdata = reader.GetOutput().GetPointData()
 
-# ...and of the cell scalars
-ca,cb = reader.GetOutput().GetCellData().GetScalars().GetRange()
-cnm =  reader.GetOutput().GetCellData().GetScalars().GetName()
-print "Range of %s: %4.2f-%4.2f" %(cnm,ca,cb)
+# show how to print a string in python
+print "O Hi!" 
+print "Points: %s" %(pointdata)
 
-# transfer function (lookup table) for mapping point scalar data
-# to colors (parent class is vtkScalarsToColors)
-lut = vtk.vtkColorTransferFunction()
-lut.AddRGBPoint(pa,           0.0, 0.0, 1.0)
-lut.AddRGBPoint(pa+(pb-pa)/4, 0.0, 0.5, 0.5)
-lut.AddRGBPoint(pa+(pb-pa)/2, 0.0, 1.0, 0.0)
-lut.AddRGBPoint(pb-(pb-pa)/4, 0.5, 0.5, 0.0)
-lut.AddRGBPoint(pb,           1.0, 0.0, 0.0)
 
-# glyphs for vector data
-glyphs = vtk.vtkGlyph3D()
-glyphs.SetInput(reader.GetOutput())
-arrow = vtk.vtkArrowSource() # the geometry used
-arrow.SetTipRadius(.05)
-arrow.SetShaftRadius(.025)
-glyphs.SetSource(0,arrow.GetOutput())
-glyphs.SetVectorModeToUseVector()     # use vector magnitude
-glyphs.SetScaleModeToScaleByVector()  # for scaling 
-glyphs.SetScaleFactor(0.5)            # global scaling
-glyphs.SetColorModeToColorByScalar()  # color by point scalars
-glyphMapper = vtk.vtkPolyDataMapper()
-glyphMapper.SetLookupTable(lut)
-glyphMapper.SetInput(glyphs.GetOutput())
-glyphActor = vtk.vtkActor()
-glyphActor.SetMapper(glyphMapper)
+array = reader.GetOutput().GetCellLocationsArray()
+for i in range(15):
+  print reader.GetOutput().GetCell(i)
+  print reader.GetOutput().GetCell(i).GetNumberOfPoints()
 
-# lookup table for cell data
-lut2 = vtk.vtkLookupTable()
-lut2.SetHueRange(0.667, 0.667)
-lut2.SetSaturationRange(0.0, 1.0)
-lut2.SetValueRange(0.8, 1.0)
-lut2.SetAlphaRange(1.0, 0.2)
-lut2.SetTableRange(ca,cb)
 
-# we want to use transparency here, then we need to sort polygons
-# to get it correct
-sorter = vtk.vtkDepthSortPolyData()
-sorter.SetInput(reader.GetOutput())
-# ...and then we need a camera to get a direction to depth sort in
-camera = vtk.vtkCamera()
-sorter.SetCamera(camera)
-
-# mapper
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetLookupTable(lut2)
-mapper.SetScalarRange(ca,cb);
-mapper.SetInput(sorter.GetOutput())
-# here we use the cell data
-mapper.SetScalarModeToUseCellData()
+# the mapper that will use the lookup table 
+#mapper = vtk.vtkPolyDataMapper()
+mapper = vtk.vtkDataSetMapper()
+mapper.SetInput(reader.GetOutput()) # connection
+# important! tell which data you want to use
+# here we use the point data
+mapper.SetScalarModeToUsePointData() 
+mapper.SetScalarRange(0,14)
 
 # the actor
 myActor = vtk.vtkActor()
 myActor.SetMapper( mapper )
 
-# a colorbar for point scalars
-scalarBar = vtk.vtkScalarBarActor()
-scalarBar.SetLookupTable( lut )
-scalarBar.SetTitle("Point scalar value")
-scalarBar.SetOrientationToHorizontal()
-scalarBar.GetLabelTextProperty().SetColor(0,0,1)
-scalarBar.GetTitleTextProperty().SetColor(0,0,1)
-
-# position it in window
-coord1 = scalarBar.GetPositionCoordinate()
-coord1.SetCoordinateSystemToNormalizedViewport()
-coord1.SetValue(0.1,0.05)
-scalarBar.SetWidth(.8)
-scalarBar.SetHeight(.15)
-
-# another colorbar for cell scalars
-scalarBar2 = vtk.vtkScalarBarActor()
-scalarBar2.SetLookupTable( mapper.GetLookupTable() )
-scalarBar2.SetTitle("Cell scalar value")
-scalarBar2.SetOrientationToHorizontal()
-scalarBar2.GetLabelTextProperty().SetColor(0,0,1)
-scalarBar2.GetTitleTextProperty().SetColor(0,0,1)
-
-# position it in window
-coord2 = scalarBar2.GetPositionCoordinate()
-coord2.SetCoordinateSystemToNormalizedViewport()
-coord2.SetValue(0.1,0.85)
-scalarBar2.SetWidth(.8)
-scalarBar2.SetHeight(.15)
-
 # renderer and render window 
 ren = vtk.vtkRenderer()
-ren.SetBackground(1, 1, 1)
+ren.SetBackground(0, 0, 0)
 renWin = vtk.vtkRenderWindow()
-renWin.SetSize(512, 512)
+renWin.SetSize(300, 300)
 renWin.AddRenderer( ren )
 
 # render window interactor
@@ -114,12 +45,7 @@ iren = vtk.vtkRenderWindowInteractor()
 iren.SetRenderWindow( renWin )
 
 # add the actors to the renderer
-ren.AddActor( glyphActor )
 ren.AddActor( myActor )
-ren.AddActor( scalarBar )
-ren.AddActor( scalarBar2 )
-ren.SetActiveCamera(camera) # add camera
-ren.ResetCamera()
 
 # render
 renWin.Render()
