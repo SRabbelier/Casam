7ee8a22b94722b58b2e91be11c58b1e6f9110103
svanberkel <svanberkel@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-05-18 13:46:45 +0000

    zoom zoom zoom
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@623 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/media/js/getmouse.js
+++ Code/casam_django/media/js/getmouse.js
@@ -7,7 +7,39 @@ function getMouseXY(e) {
     var viewportOffset = $('big_images').viewportOffset();    
     $('MouseX').value = mX-viewportOffset.left;
     $('MouseY').value = mY-viewportOffset.top;
- 
+   
+  }
+
+  if(addedImages[0]){
+	  $('zoomImage').setAttribute('src',base_path+'imageLoader/byRatio/150/'+addedImages[0].id);
+  }else{
+	  $('zoomImage').setAttribute('src','');
+  }
+  if($('zoomImage').getAttribute('src') && $('zoomImage').getAttribute('src') != '' ){
+	  var zoomImg = new Image();
+	  zoomImg.setAttribute('src',$('zoomImage').getAttribute('src'));
+	  
+	  var originalWidth = zoomImg.width / 1.5;
+	  var originalHeight = zoomImg.height / 1.5;
+	  var activeImages = $('big_images').select('.big_image_sibling');
+	  if(!activeImages)return;
+	  var activeImage = activeImages[activeImages.length-1];
+	  if(!activeImage)return;
+	  var middleImage = new Image();
+	  middleImage.setAttribute('src',activeImage.getAttribute('src'));
+	  var resizedWidth = middleImage.width;
+	  var resizedHeight = middleImage.height;
+	  
+	  var zoomRatio = (originalWidth / resizedWidth) * 1.5;
+	  var newLeft =  Math.round(-1 *  $('MouseX').value * zoomRatio + 90);
+	  var newTop =  Math.round(-1 *  $('MouseY').value * zoomRatio + 90);
+	  $('zoomImage').setStyle({
+		  marginLeft:newLeft+'px',
+		  marginTop:newTop+'px'
+	  });
+	  
+	  
+	  
   }
 
 }
