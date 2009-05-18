71cf0fed5cbf0cba02ebf5d84efbef8f11511c8f
nnsmit <nnsmit@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-05-18 09:59:01 +0000

    Mooie cleanup.
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@614 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/casam/logic/point_distribution_model.py
+++ Code/casam_django/casam/logic/point_distribution_model.py
@@ -1,230 +1,175 @@
+import colorsys
+import logging
+
 import vtk
 
-class PointDistributionModel:
+
+def getColors(colors):
+  """
+  """
+
+  return [colorsys.hsv_to_rgb(x*1.0/colors, 1, 1) for x in range(colors)]
+
+
+class PointDistributionModel(object):
   """Calculates the Point Distribution Model using an unstructured grid
   """
   
-  # Skip the reading of data for now, might as well get it directly from the database
-  # our data source, a PolyDataReader
-  #reader = vtk.vtkPolyDataReader()
-  #reader = vtk.vtkUnstructuredGridReader()
-  #reader.SetFileName("H:/desktop/landmarks.vtk")
-  #reader.Update() # by calling Update() we read the file
+  def __init__(self):
+    """
+    """
+
+    self.filterGPA = vtk.vtkProcrustesAlignmentFilter()
+    self.filterPCA = vtk.vtkPCAAnalysisFilter()
+
+    self.renderer = vtk.vtkRenderer()
+    self.renderer.SetBackground(0, 0, 0)
+
+    self.renderWindow = vtk.vtkRenderWindow()
+    self.renderWindow.SetSize(300, 300)
+    self.renderWindow.AddRenderer( self.renderer )
   
-  #pointdata = reader.GetOutput().GetPointData()
+    # render window interactor
+    self.windowInteractor = vtk.vtkRenderWindowInteractor()
+    self.windowInteractor.SetRenderWindow(self.renderWindow)
+
+    self.vertexGrids = []
+    self.alignedGrids = []
+    self.analyzedGrids = []
+
+    self.colors = getColors(10)
+
+  def addPointSet(self, data):
+    """
+    """
+
+    logging.info("adding point set")
+
+    size = len(data)
+
+    # Create some landmarks, put them in UnstructuredGrid
+    # Start off with some landmarks
+    vertexPoints = vtk.vtkPoints()
+    vertexPoints.SetNumberOfPoints(size)
+
+    vertexGrid = vtk.vtkUnstructuredGrid()
+    vertexGrid.Allocate(size, size)
+
+    for id, (x, y, z) in enumerate(data):
+      vertexPoints.InsertPoint(id, x, y, z)
+
+      # Create vertices from them
+      vertex = vtk.vtkVertex()
+      vertex.GetPointIds().SetId(0, id)
+
+      #Create an unstructured grid with the landmarks added
+      vertexGrid.InsertNextCell(vertex.GetCellType(), vertex.GetPointIds())
   
-  # filters we want to use
-  filterGPA = vtk.vtkProcrustesAlignmentFilter()
-  filterPCA = vtk.vtkPCAAnalysisFilter()
+    vertexGrid.SetPoints(vertexPoints)
+    self.vertexGrids.append(vertexGrid)
+    
+    logging.info("done")
   
+  def addActors(self, grids, colorid):
+    """
+    """
+
+    logging.info("adding actors")
+
+    for id, grid in enumerate(grids):
+      #Create a mapper for our data
+      vertexMapper = vtk.vtkDataSetMapper()
+      vertexMapper.SetInput(grid)
+      
+      #Create the actor
+      vertexActor = vtk.vtkActor()
+      vertexActor.SetMapper(vertexMapper)
+      vertexActor.GetProperty().SetDiffuseColor(self.colors[colorid])
+      
+      # add the actors to the renderer
+      self.renderer.AddActor( vertexActor )
+    
+    logging.info("done")
+
+  def procrustes(self):
+    """
+    """
+
+    logging.info("running procrustes")
+
+    size = len(self.vertexGrids)
+
+    self.filterGPA.SetNumberOfInputs(size)
+
+    for id, grid in enumerate(self.vertexGrids):
+      self.filterGPA.SetInput(id, grid)
+      
+    self.filterGPA.Update()
+
+    for id in range(size):
+      grid = self.filterGPA.GetOutput(id)
+      self.alignedGrids.append(grid)
+      
+    logging.info("done")
   
-  # Create some landmarks, put them in UnstructuredGrid
-  # Start off with some landmarks
-  vertexPoints = vtk.vtkPoints()
-  vertexPoints.SetNumberOfPoints(3)
-  vertexPoints.InsertPoint(0, 0, 0, 0)
-  vertexPoints.InsertPoint(1, 3, 1, 0)
-  vertexPoints.InsertPoint(2, 2, 2, 0)
+  def pca(self):
+    """
+    """
+
+    logging.info("running pca")
+
+    size = len(self.alignedGrids)
+
+    self.filterPCA.SetNumberOfInputs(size)
+    
+    for id, grid in enumerate(self.alignedGrids):    
+      self.filterPCA.SetInput(id, grid)
+
+    self.filterGPA.Update()
+
+    for id in range(size):
+      grid = self.filterPCA.GetOutput(id)
+      self.analyzedGrids.append(grid)
+
+    logging.info("done")
+
+  def render(self):
+    """
+    """
+
+    logging.info("rendering")
+    
+    self.renderWindow.Render()
+    
+    # initialize and start the interactor
+    self.windowInteractor.Initialize()
+    self.windowInteractor.Start()
+    
+    logging.info("done")
+
+
+def main():
+  logging.basicConfig(level=logging.DEBUG)
+  pdm = PointDistributionModel()
+
+  pdm.addPointSet([
+      (0, 0, 0), 
+      (3, 1, 0), 
+      (2, 2, 0),
+      ])
   
-  # Create vertices from them
-  aVertexa = vtk.vtkVertex()
-  aVertexa.GetPointIds().SetId(0, 0)
-  aVertexb = vtk.vtkVertex()
-  aVertexb.GetPointIds().SetId(0, 1)
-  aVertexc = vtk.vtkVertex()
-  aVertexc.GetPointIds().SetId(0, 2)
-  
-  #Create an unstructured grid with the landmarks added
-  aVertexGrid = vtk.vtkUnstructuredGrid()
-  aVertexGrid.Allocate(3, 3)
-  aVertexGrid.InsertNextCell(aVertexa.GetCellType(), aVertexa.GetPointIds())
-  aVertexGrid.InsertNextCell(aVertexb.GetCellType(), aVertexb.GetPointIds())
-  aVertexGrid.InsertNextCell(aVertexc.GetCellType(), aVertexc.GetPointIds())
-  aVertexGrid.SetPoints(vertexPoints)
-  
-  #Create a mapper for our data
-  aVertexMapper = vtk.vtkDataSetMapper()
-  aVertexMapper.SetInput(aVertexGrid)
-  
-  #Create the actor
-  aVertexActor = vtk.vtkActor()
-  aVertexActor.SetMapper(aVertexMapper)
-  aVertexActor.GetProperty().SetDiffuseColor(1, 0.5, 1)
-  
-  
-  #Now for set number 2
-  # Start off with some landmarks
-  vertexPoints2 = vtk.vtkPoints()
-  vertexPoints2.SetNumberOfPoints(3)
-  vertexPoints2.InsertPoint(0, 0.7, 0, 0)
-  vertexPoints2.InsertPoint(1, 3.5, 1, 0)
-  vertexPoints2.InsertPoint(2, 2.3, 2, 0)
-  
-  # Create vertices from them
-  aVertexa2 = vtk.vtkVertex()
-  aVertexa2.GetPointIds().SetId(0, 0)
-  aVertexb2 = vtk.vtkVertex()
-  aVertexb2.GetPointIds().SetId(0, 1)
-  aVertexc2 = vtk.vtkVertex()
-  aVertexc2.GetPointIds().SetId(0, 2)
-  
-  #Create an unstructured grid with the landmarks added
-  aVertexGrid2 = vtk.vtkUnstructuredGrid()
-  aVertexGrid2.Allocate(3, 3)
-  aVertexGrid2.InsertNextCell(aVertexa2.GetCellType(), aVertexa2.GetPointIds())
-  aVertexGrid2.InsertNextCell(aVertexb2.GetCellType(), aVertexb2.GetPointIds())
-  aVertexGrid2.InsertNextCell(aVertexc2.GetCellType(), aVertexc2.GetPointIds())
-  aVertexGrid2.SetPoints(vertexPoints2)
-  
-  #Create a mapper for our data
-  aVertexMapper2 = vtk.vtkDataSetMapper()
-  aVertexMapper2.SetInput(aVertexGrid2)
-  
-  #Create the actor
-  aVertexActor2 = vtk.vtkActor()
-  aVertexActor2.SetMapper(aVertexMapper2)
-  aVertexActor2.GetProperty().SetDiffuseColor(0.5, 1, 1)
-  
-  
-  #Perform some Procrustes
-  #filterGPA.GetLandmarkTransform().SetModeToRigidBody()
-  filterGPA.SetNumberOfInputs(2)
-  filterGPA.SetInput(0,aVertexGrid)
-  filterGPA.SetInput(1,aVertexGrid2)
-  print "Performing Generalized Procrustes Analysis..."
-  filterGPA.Update()
-  print "Done!"
-  GPAGrid = filterGPA.GetOutput(0)
-  GPAGrid2 = filterGPA.GetOutput(1)
-  
-    #Create a mapper for our data
-  aVertexMapperGPA = vtk.vtkDataSetMapper()
-  aVertexMapperGPA.SetInput(GPAGrid)
-  #Create the actor
-  aVertexActorGPA = vtk.vtkActor()
-  aVertexActorGPA.SetMapper(aVertexMapperGPA)
-  aVertexActorGPA.GetProperty().SetDiffuseColor(1, 0, 0)
-  
-  #Create a mapper for our data
-  aVertexMapperGPA2 = vtk.vtkDataSetMapper()
-  aVertexMapperGPA2.SetInput(GPAGrid2)
-  #Create the actor
-  aVertexActorGPA2 = vtk.vtkActor()
-  aVertexActorGPA2.SetMapper(aVertexMapperGPA2)
-  aVertexActorGPA2.GetProperty().SetDiffuseColor(0, 1, 0)
-  
-  #Also some PCA
-  filterPCA.SetNumberOfInputs(2)
-  filterPCA.SetInput(0,GPAGrid)
-  filterPCA.SetInput(1,GPAGrid2)
-  print "Performing Principal Component Analysis..."
-  filterPCA.Update()
-  print "Done!"
-  PCAGrid = filterPCA.GetOutput(0)
-  PCAGrid2 = filterPCA.GetOutput(1)
-  
-    #Create a mapper for our data
-  aVertexMapperPCA = vtk.vtkDataSetMapper()
-  aVertexMapperPCA.SetInput(PCAGrid)
-  #Create the actor
-  aVertexActorPCA = vtk.vtkActor()
-  aVertexActorPCA.SetMapper(aVertexMapperPCA)
-  aVertexActorPCA.GetProperty().SetDiffuseColor(1, 1, 0)
-  
-  #Create a mapper for our data
-  aVertexMapperPCA2 = vtk.vtkDataSetMapper()
-  aVertexMapperPCA2.SetInput(PCAGrid2)
-  #Create the actor
-  aVertexActorPCA2 = vtk.vtkActor()
-  aVertexActorPCA2.SetMapper(aVertexMapperPCA2)
-  aVertexActorPCA2.GetProperty().SetDiffuseColor(0, 1, 1)
-  
-  
-  #polyDataSet = vtk.vtkPolyData()
-  
-  #vertexPointsa = vtk.vtkPoints()
-  #vertexPointsa.SetNumberOfPoints(1)
-  #vertexPointsa.InsertPoint(0, 0, 0, 0)
-  
-  #vertexPointsb = vtk.vtkPoints()
-  #vertexPointsb.SetNumberOfPoints(1)
-  #vertexPointsb.InsertPoint(0, 3, 1, 0)
-  
-  #vertexPointsc = vtk.vtkPoints()
-  #vertexPointsc.SetNumberOfPoints(1)
-  #vertexPointsc.InsertPoint(0, 2, 2, 0)
-  
-  #aVertexa = vtk.vtkVertex()
-  #aVertexa.GetPointIds().SetId(0, 0)
-  #aVertexb = vtk.vtkVertex()
-  #aVertexb.GetPointIds().SetId(0, 1)
-  #aVertexc = vtk.vtkVertex()
-  #aVertexc.GetPointIds().SetId(0, 2)
-  
-  
-  #verts = vtk.vtkCellArray()
-  #verts.InsertNextCell(aVertexa)
-  #verts.InsertNextCell(aVertexb)
-  #verts.InsertNextCell(aVertexc)
-  #polyDataSet.SetVerts(verts)
-  
-  #polyDataSet.Allocate(3, 3)
-  #polyDataSet.InsertNextCell(aVertexa.GetCellType(), aVertexa.GetPointIds())
-  #polyDataSet.InsertNextCell(aVertexb.GetCellType(), aVertexb.GetPointIds())
-  #polyDataSet.InsertNextCell(aVertexc.GetCellType(), aVertexc.GetPointIds())
-  
-  #VertexMapper = vtk.vtkPolyDataMapper()
-  #VertexMapper.SetInput(polyDataSet)
-  
-  #aVertexActor = vtk.vtkActor()
-  #aVertexActor.SetMapper(aVertexMapper)
-  #aVertexActor.AddPosition(0, 0, 0)
-  #aVertexActor.GetProperty().SetDiffuseColor(1, 1, 1)
-  
-  
-  #array = reader.GetOutput().GetCellLocationsArray()
-  #for i in range(15):
-  #  tuple = reader.GetOutput().GetCell(i).GetBounds()
-  #  print "Coordinates: ", tuple[0], tuple[2]
-  #  print reader.GetOutput().GetCell(i).GetPointIds()
-  
-  
-  # the mapper that will use the lookup table 
-  #mapper = vtk.vtkPolyDataMapper()
-  #mapper = vtk.vtkDataSetMapper()
-  #mapper.SetInput(reader.GetOutput()) # connection
-  # important! tell which data you want to use
-  # here we use the point data
-  #mapper.SetScalarModeToUsePointData() 
-  #mapper.SetScalarRange(0,4)
-  
-  # the actor
-  #myActor = vtk.vtkActor()
-  #myActor.SetMapper( mapper )
-  
-  # renderer and render window 
-  ren = vtk.vtkRenderer()
-  ren.SetBackground(0, 0, 0)
-  renWin = vtk.vtkRenderWindow()
-  renWin.SetSize(300, 300)
-  renWin.AddRenderer( ren )
-  
-  # render window interactor
-  iren = vtk.vtkRenderWindowInteractor()
-  iren.SetRenderWindow( renWin )
-  
-  # add the actors to the renderer
-  #ren.AddActor( myActor )
-  ren.AddActor( aVertexActor )
-  ren.AddActor( aVertexActor2 )
-  ren.AddActor( aVertexActorGPA )
-  ren.AddActor( aVertexActorGPA2 )
-  ren.AddActor( aVertexActorPCA )
-  ren.AddActor( aVertexActorPCA2 )
-  
-  # render
-  renWin.Render()
-  
-  # initialize and start the interactor
-  iren.Initialize()
-  iren.Start()
+  pdm.addPointSet([
+      (0.7, 0, 0), 
+      (3.5, 1, 0), 
+      (2.3, 2, 0),
+      ])
+
+  pdm.procrustes()
+  pdm.pca()
+  pdm.addActors(pdm.vertexGrids, 0)
+  pdm.addActors(pdm.alignedGrids, 2)
+  pdm.addActors(pdm.analyzedGrids, 4)
+  pdm.render()
+
+if __name__ == '__main__':
+  main()
