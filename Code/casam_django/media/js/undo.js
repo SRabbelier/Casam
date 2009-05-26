0b6415d4318b47f102cb9c867b71b44ca4573c11
bmwsedee <bmwsedee@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-05-26 14:22:52 +0000

    * fix undo changes * when there are no changes, no changes can be undone
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@705 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/media/js/undo.js
+++ Code/casam_django/media/js/undo.js
@@ -1,88 +1,87 @@
-var Change = Class
-		.create( {
-			initialize : function(type, lmid, lmname) {
-				//type: p is for positioning landmarks, r is for repositioning landmarks
-				this.type = type;
-				this.potid = lmid;
-				this.lmname = lmname;
-				this.metingid = '';
-				this.oldx = '';
-				this.oldy = '';
-				this.saved = false;
-				this.changeDiv = '';
-				this.imageid = addedImages[0].id;
-				this.saveButton = '';
-				this.posx = '';
-				this.posy = '';
-			},
-			reposition : function(mid, x, y) {
-				this.metingid = mid;
-				this.oldx = this.posx;
-				this.oldy = this.posy;
-				this.posx = x;
-				this.posy = y;
-			},
-			position : function(x, y) {
-				this.posx = x;
-				this.posy = y;
-			},
-			forceSave : function() {
-				if (!this.saved) {
-					this.changeDiv.childElements()[1].remove();
-					this.saveButton = '';
-				}
-				this.saved = true;
-				this.changeDiv.removeClassName('unsavedChangeDiv');
-				this.changeDiv.addClassName('savedChangeDiv');
-			},
-			save : function() {
-				this.forceSave();
-				mid = this.metingid;
-				changes.each( function(item) {
-					if (item.metingid == mid) {
-						item.forceSave();
-					}
-				});
-			},
-			add : function() {
-				this.changeDiv = new Element('div');
-				if (this.saved)
-					this.changeDiv.addClassName('savedChangeDiv');
-				else
-					this.changeDiv.addClassName('unsavedChangeDiv');
-				var span = new Element('span');
-				if (this.type == 'r')
-					span.update('Landmark ' + this.lmname + ' has moved from ('
-							+ this.oldx + ',' + this.oldy + ') to ('
-							+ this.posx + ',' + this.posy + ')')
-				else
-					span.update('Landmark ' + this.lmname
-							+ ' has been placed at (' + this.posx + ','
-							+ this.posy + ')');
-				this.changeDiv.insert(span);
-				if (!this.saved) {
-					var saveButton = new Element('a', {
-						'href' : '#'
-					});
-					saveButton.addClassName('saveChangeButton');
-					saveButton.update('Save this change');
-					this.saveButton = saveButton;
-					this.changeDiv.insert(saveButton);
-				}
-				$('ajax_result').insert(this.changeDiv);
-				watchSaveButton(this);
-			},
-			undo : function() {
-				$('ajax_result').childElements()[$('ajax_result')
-						.childElements().length - 1].remove();
-				undoLastLandmarkChange(this.oldx, this.oldy, this.potid,
-						this.imageid, this.metingid);
-				changes.pop();
-			},
-			erase : function() {
-				this.changeDiv.remove();
+var Change = Class.create( {
+	initialize : function(type, lmid, lmname) {
+		//type: p is for positioning landmarks, r is for repositioning landmarks
+		this.type = type;
+		this.potid = lmid;
+		this.lmname = lmname;
+		this.metingid = '';
+		this.oldx = '';
+		this.oldy = '';
+		this.saved = false;
+		this.changeDiv = '';
+		this.imageid = addedImages[0].id;
+		this.saveButton = '';
+		this.posx = '';
+		this.posy = '';
+	},
+	reposition : function(mid, x, y) {
+		this.metingid = mid;
+		this.oldx = this.posx;
+		this.oldy = this.posy;
+		this.posx = x;
+		this.posy = y;
+	},
+	position : function(x, y) {
+		this.posx = x;
+		this.posy = y;
+	},
+	forceSave : function() {
+		if (!this.saved) {
+			this.changeDiv.childElements()[1].remove();
+			this.saveButton = '';
+		}
+		this.saved = true;
+		this.changeDiv.removeClassName('unsavedChangeDiv');
+		this.changeDiv.addClassName('savedChangeDiv');
+	},
+	save : function() {
+		this.forceSave();
+		mid = this.metingid;
+		changes.each( function(item) {
+			if (item.metingid == mid) {
+				item.forceSave();
 			}
 		});
+	},
+	add : function() {
+		this.changeDiv = new Element('div');
+		if (this.saved)
+			this.changeDiv.addClassName('savedChangeDiv');
+		else
+			this.changeDiv.addClassName('unsavedChangeDiv');
+		var span = new Element('span');
+		if (this.type == 'r')
+			span.update('Landmark ' + this.lmname + ' has moved from ('
+					+ this.oldx + ',' + this.oldy + ') to ('
+					+ this.posx + ',' + this.posy + ')')
+		else
+			span.update('Landmark ' + this.lmname
+					+ ' has been placed at (' + this.posx + ','
+					+ this.posy + ')');
+		this.changeDiv.insert(span);
+		if (!this.saved) {
+			var saveButton = new Element('a', {
+				'href' : '#'
+			});
+			saveButton.addClassName('saveChangeButton');
+			saveButton.update('Save this change');
+			this.saveButton = saveButton;
+			this.changeDiv.insert(saveButton);
+		}
+		$('ajax_result').insert(this.changeDiv);
+		watchSaveButton(this);
+	},
+	undo : function() {
+		$('ajax_result').childElements()[$('ajax_result')
+				.childElements().length - 1].remove();
+		undoLastLandmarkChange(this.oldx, this.oldy, this.potid,
+				this.imageid, this.metingid);
+		changes.pop();
+	},
+	erase : function() {
+		this.changeDiv.remove();
+	}
+});
 function reloadUndoneChange(metingid, posx, posy) {
 	var pin = ''
 	for ( var i = 0; i < measurements.length; i++) {
@@ -94,15 +93,9 @@ function reloadUndoneChange(metingid, posx, posy) {
 	pin.calcpieces();
 	pin.setPlace(posx / pin.piecex, posy / pin.piecey);
 	pin.place();
-	var currentMeasurements = $('bottomDiv' + pin.imageid).childElements()[1]
-			.childElements()[2].childElements();
-	for ( var i = 0; i < currentMeasurements.length; i++) {
-		if (currentMeasurements[i].childElements()[0].name == pin.id) {
-			currentMeasurements[i].childElements()[1].update(pin.name + ' ('
-					+ Math.round(pin.x) + ',' + Math.round(pin.y) + ')');
-			break;
-		}
-	}
+	console.log($('potidMeasDiv_'+pin.potid));
+	new Effect.Highlight($('potidMeasDiv_'+pin.potid));
+  $('span_'+pin.potid).update(pin.name + ' (' + Math.round(pin.x) + ',' + Math.round(pin.y) + ')');
 }
 
 function reloadUndonePlace(potid, imageID) {
@@ -117,6 +110,7 @@ function reloadUndonePlace(potid, imageID) {
 					'imageID' : imageID
 				},
 				onSuccess : function(transport, json) {
+					// Remove measurement pin
 					var pin = ''
 					for ( var i = 0; i < measurements.length; i++) {
 						if ((measurements[i].potid == potid)
@@ -127,22 +121,12 @@ function reloadUndonePlace(potid, imageID) {
 						}
 					}
 					pin.restore();
-					measurementDescriptionOffset = $('mm' + potid).up().down(
-							"div[class='mmText']").positionedOffset();
-					$('mm' + potid).setStyle( {
-						'left' : (measurementDescriptionOffset[0] - 12) + 'px',
-						'top' : (measurementDescriptionOffset[1]) + 'px'
-					});
-
-					var currentMeasurements = $('bottomDiv' + imageID)
-							.childElements()[1].childElements()[2]
-							.childElements();
-					for ( var i = 0; i < currentMeasurements.length; i++) {
-						if (currentMeasurements[i].childElements()[0].name == pin.id) {
-							currentMeasurements[i].remove();
-							break;
-						}
-					}
+					// Restore potential measurement pin
+					$('mm' + potid).show();
+					
+					// Remove measurement text
+					Effect.Fade($('potidMeasDiv_'+potid));
+					$('potidMeasDiv_'+potid).remove();
 				},
 				onFailure : function() {
 					alert('Something went wrong while restoring measurement');
@@ -151,6 +135,8 @@ function reloadUndonePlace(potid, imageID) {
 }
 
 function undoLastChange() {
-	var c = changes[changes.length - 1];
-	c.undo();
+	if (changes.length > 0){
+		var c = changes[changes.length - 1];
+		c.undo();
+	}
 }
