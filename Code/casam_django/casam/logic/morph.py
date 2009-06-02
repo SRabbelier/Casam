6b0807523497be989e59e51e097add9f7c9815e2
nnsmit <nnsmit@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-06-02 13:49:16 +0000

    DO NOT EAT! highly experimental! work in progress ^^
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@764 9b22acdf-97ab-464f-81e2-08fcc4a6931f

---
+++ Code/casam_django/casam/logic/morph.py
@@ -0,0 +1,100 @@
+import vtk
+
+from casam.models import OriginalImage
+from casam.models import Measurement
+from casam.models import PotentialMeasurement
+from casam.models import Project
+from casam.models import PDM
+from django.conf import settings
+# read in coordinates
+
+def createMorph(selectedImages,selectedPMs):
+  '''
+  Create the Point Distribution Model from the selected measurements
+  '''
+  potentialids = []
+  totalcoordsx = []
+  totalcoordsy = []
+  #coordinates aan de hand selected measurements goed opbouwen in vtkpoints
+  for i in range(len(selectedPMs)):#for the number of images
+    measures = Measurement.objects.all().filter(id__in=selectedPMs[i])#get the measurements
+    coordsx = []
+    coordsy = []
+    
+    for k in range(len(measures)):#for every measurement in the measurements
+      measurement = measures[k]
+      if i == 0: #to check if the measurements come from the same potentialmeasurements
+        potentialids.append(measurement.mogelijkemeting.id)#on first run, store
+      elif potentialids[k] != measurement.mogelijkemeting.id:#on second run compare
+        return pdmodel, 0
+      coordsx.append(float(measurement.x))
+      coordsy.append(float(measurement.y))
+      
+    totalcoordsx.append(coordsx)
+    totalcoordsy.append(coordsy)
+  
+  totalImages = []
+  for i in range(len(selectedImages)):
+    totalImages.append(OriginalImage.objects.all().get(id=selectedImages[i]))
+  
+  totalcoords = []
+  first = True
+  while len(totalImages) != 1:
+    if first:
+      r1 = vtk.vtkJPEGReader()
+      r1.SetFileName(DATADIR + totalImages[0].path)
+      r1.Update()
+    else:
+      r1 = totalImages[0]
+    
+    
+    ydim1 = r1.GetOutput().GetDimensions()[1]
+    totalcoords[0] = [(x, ydim1 - y, 0) for (x,y) in zip(totalcoordsx[0], totalcoordsy[0])]
+    
+    r2 = vtk.vtkJPEGReader()
+    r2.SetFileName(DATADIR + totalImages[1].path)
+    r2.Update()
+    ydim2 = r2.GetOutput().GetDimensions()[1]
+    totalcoords[1] = [(x, ydim2 - y, 0) for (x,y) in zip(totalcoordsx[1], totalcoordsy[1])]
+    
+    # convert everything to vtkPoints
+    coords = [totalcoords[0], totalcoords[1]]
+    lms = [vtk.vtkPoints() for _ in range(2)]
+    
+    for idx, c in enumerate(coords):
+        lms[idx].SetNumberOfPoints(len(c))
+        for ptidx, pt in enumerate(c):
+            lms[idx].SetPoint(ptidx, pt)
+    
+    # setup the transform
+    tps = vtk.vtkThinPlateSplineTransform()
+    tps.SetBasisToR2LogR()
+    tps.SetSourceLandmarks(lms[0])
+    lms[0].Modified()
+    tps.SetTargetLandmarks(lms[1])
+    lms[1].Modified()
+    # the tps transforms from lms[0] to lms[1] (see the docs)
+    # but we're going to apply this as a reslice transform, which means
+    # the grid and not the coordinates is transformed, which means we need
+    # the inverse.  This is slow.
+    tps.Inverse()
+    tps.Update()
+    
+    # then transform
+    ir = vtk.vtkImageReslice()
+    # use cubic for highest quality (slower)
+    # we're using linear here to shave off a few microseconds. :)
+    ir.SetInterpolationModeToLinear()
+    ir.SetResliceTransform(tps)
+    ir.SetInput(r1.GetOutput())
+    ir.SetInformationInput(r2.GetOutput())
+        
+    totalImages.pop(0)
+    totalImages.pop(0)
+    
+    totalImages.insert(0, ir)
+    first = False
+    
+  return True, 1
+  #images readen aan de hand selectedImages
+  
---
+++ Code/casam_django/casam/views/morph.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+from django import http
+from django.template import loader
+from casam.views import handler
+from casam.logic import morph
+from django import forms
+from django.utils import simplejson
+
+class MorphForm(forms.Form):
+  projectID = forms.CharField(max_length=36)
+  pdmData = forms.CharField()
+    
+class MorphCreator(handler.Handler):
+
+  def getPostForm(self):
+    return MorphForm(self.POST)
+
+  def post(self):
+    projectID = self.cleaned_data['projectID']
+    pdmData = self.cleaned_data['pdmData']
+    pdmDataObject = simplejson.loads(pdmData)['pdmData']
+    if (len(pdmDataObject)==0):
+      return http.HttpResponseServerError('Not enough landmarks selected.')
+    images = []
+    measurements = []
+    measurecount = len((pdmDataObject[0])[1])
+    for i in range(len(pdmDataObject)):
+      imagemeasure = pdmDataObject[i]
+      if len(imagemeasure[1]) != measurecount or len(imagemeasure[1]) == 0:
+        return http.HttpResponseServerError('Incorrect number of landmarks per image selected.') 
+      images.append(imagemeasure[0])
+    morphmodel, result = morph.createMorph(images,measurements)
+    if result == 0:
+      return http.HttpResponseServerError('Landmarks are not comparable.') 
+    #morph.analyse(pdmodel, projectID, images)
+    return http.HttpResponse('Successfully saved Point Distribution Model overlay-image to data directory.')
+
+  
+  def get(self):
+    return http.HttpResponse('success')
