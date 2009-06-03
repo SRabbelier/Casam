890061c65167c8faf200cf0a292971acc8d7c149
bmwsedee <bmwsedee@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-06-03 13:02:15 +0000

    toggle checkbox 'shape defining'
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@794 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/casam/logic/potential_measurement.py
+++ Code/casam_django/casam/logic/potential_measurement.py
@@ -4,6 +4,8 @@ from casam.models import PotentialMeasurementType
 
 def handle_add_potential_measurement(project, type, soort, name, shape):
   if PotentialMeasurement.objects.all().filter(project=project).filter(type=type).filter(name=name).count() == 0:
+    if (soort == 'B'):
+      shape = False
     pm = PotentialMeasurement(name=name, type=type, soort=soort, project=project, shapedefining=shape)
     pm.save()
     return pm
