7986e52ceeba9834a61ccfb9fad5142355e81acf
nnsmit <nnsmit@9b22acdf-97ab-464f-81e2-08fcc4a6931f>
2009-06-07 21:04:15 +0000

    How was my weekend you ask? This was my weekend ;)
    git-svn-id: https://graphics.tudelft.nl/svn/BachCASAM@857 9b22acdf-97ab-464f-81e2-08fcc4a6931f

--- Code/casam_django/casam/logic/pdm.py
+++ Code/casam_django/casam/logic/pdm.py
@@ -8,38 +8,6 @@ from casam.logic import point_distribution_model as pdm
 from django.conf import settings
 from PIL import Image
 
-def tempPDM():
-  '''
-  Temporary PDM Creation without providing project, selected possible measurements and selected images
-  '''
-  pdmodel = pdm.makePDM()
-  project = Project.objects.all()[0] 
-  potentials = PotentialMeasurement.objects.all().filter(project=project)
-
-  totalmeasure = 0
-  measurementcount = len(Measurement.objects.select_related().filter(mogelijkemeting=potentials[0]))
-  for measure in range(measurementcount):
-    coords = []
-    for pot in potentials:
-      measurements = Measurement.objects.select_related().filter(mogelijkemeting=pot)
-      if totalmeasure == 0:
-        totalmeasure = len(measurements)
-        coords.append((float(measurements[measure].x),float(measurements[measure].y),0.0))
-      elif totalmeasure == len(measurements): 
-        coords.append((float(measurements[measure].x),float(measurements[measure].y),0.0))
-      else:
-        totalmeasure = 0
-        break   
-    if totalmeasure != 0:
-      pdmodel.addPointSet(coords)
-    else:
-      #raise some kind of exception here somehow!
-      pdmodel = pdm.makePDM()
-      pdmodel.addPointSet([(100,100,0)])
-      pdmodel.addPointSet([(250,150,0)])
-      break
-  return pdmodel
-
 def createPDM(selectedImages,selectedPMs):
   """
   Create the Point Distribution Model from the selected measurements
